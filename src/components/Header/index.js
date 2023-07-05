07fe631eb14c1d2c5497033a65baa00c9726cc9f
Ruan Silva <progmruansilva@gmail.com>
2023-07-05 15:38:06 -0300

    chor:header inicial, faltando finalizar

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./header.css";
+
+export default function Header() {
+  return(
+    <div className="header">
+      <h1>Brothers Tech.Blog</h1>
+       <div className="header-section">
+        <h2>Home</h2>
+        <h2>Artigos</h2>
+        <h2>Sobre</h2>
+        <h2>Contato</h2>
+       </div>
+    </div>
+  )
+}
