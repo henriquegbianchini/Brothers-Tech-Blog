dcfd8172f4328e523436643e95eb5cba4ebe8044
Ruan Silva <progmruansilva@gmail.com>
2023-07-05 18:16:10 -0300

    chor: primeiro esquema do footer, não terminado, não alinhado

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,27 @@ import "./footer.css";
 export default function Footer() {
   return(
     <div className="footer">
-      <div className="links"></div>
-      <div className="categories"></div>
-      <div className="contact"></div>
+      <div className="contact">
+        <li>Brothers Tech.Blog</li>
+        <li>By Henrique Bianchini and Rafael Reis</li>
+        <li>brotherstechblog@gmai.com</li>
+      </div>
+      <div className="links">
+        <li className="underline">Links</li>
+        <li>Home</li>
+        <li>Sobre</li>
+        <li>Artigos</li>
+        <li>Contato</li>
+      </div>
+      <div className="categories">
+        <li className="underline">Categorias</li>
+        <li>Redes</li>
+        <li>Software</li>
+        <li>Hardware</li>
+        <li>Segurança</li>
+        <li>Computação</li>
+        <li>Progamação</li>
+      </div>
     </div>
   )
 }
