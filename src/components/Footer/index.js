c53d1f4489ff831ef8ae5ef78b6809ce1112cd53
Ruan Silva <progmruansilva@gmail.com>
2023-07-05 16:00:51 -0300

    fix: corrigindo cor do cabeçalho

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,10 @@ import "./footer.css";
 
 export default function Footer() {
   return(
-    <div>
-
+    <div className="footer">
+      <div className="links"></div>
+      <div className="categories"></div>
+      <div className="contact"></div>
     </div>
   )
 }
