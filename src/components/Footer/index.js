129687ed91c23be3af0b1bc04e6a038372987cd0
Ruan Silva <progmruansilva@gmail.com>
2023-07-10 16:42:22 -0300

    feature: adiciondando finalização do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,25 +5,25 @@ export default function Footer() {
   return(
     <div className="footer">
       <div className="contact">
-        <li>Brothers Tech.Blog</li>
+        <h3>Brothers Tech<span>.Blog</span></h3>
         <li>By Henrique Bianchini and Rafael Reis</li>
-        <li>brotherstechblog@gmai.com</li>
+        <li>brotherstechblog@gmail.com</li>
       </div>
       <div className="links">
         <li className="underline">Links</li>
-        <li>Home</li>
-        <li>Sobre</li>
-        <li>Artigos</li>
-        <li>Contato</li>
+        <li className="inline">Home</li>
+        <li className="inline">Sobre</li>
+        <li className="inline">Artigos</li>
+        <li className="inline">Contato</li>
       </div>
       <div className="categories">
         <li className="underline">Categorias</li>
-        <li>Redes</li>
-        <li>Software</li>
-        <li>Hardware</li>
-        <li>Segurança</li>
-        <li>Computação</li>
-        <li>Progamação</li>
+        <li className="inline">Redes</li>
+        <li className="inline">Software</li>
+        <li className="inline">Hardware</li>
+        <li className="inline">Segurança</li>
+        <li className="inline">Computação</li>
+        <li className="inline">Progamação</li>
       </div>
     </div>
   )
