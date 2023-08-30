5b9311668254578bf8bef5ef47a8e795dce6a97e
Ruan Silva <progmruansilva@gmail.com>
2023-08-30 01:46:46 -0300

    chore: renomeando routes para controller.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const consign = require("consign")
 
 consign()
-    .then('./routes.js')
+    .then('./controller.js')
     .into(app)
 
 console.log("api running on Localhost:5000")
