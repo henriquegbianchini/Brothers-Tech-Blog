f126c3c894df1597b5ccfff024d2a26d426591ba
Ruan Silva <progmruansilva@gmail.com>
2023-09-12 17:34:21 -0300

    ✨ (main): Renomeando pasta web
    renomei a pasta web para front-end

--- api/services/articles.js
+++ api/services/articles.js
@@ -1,9 +1,9 @@
 module.exports = app => {
 
 const get = (req, res) => {
-  // app.db('articles')
-  //   .select('name')
-  //   .then(author => res.json(author))
+  app.db('articles')
+    .select('name')
+    .then(author => res.json(author))
     res.send('articles runnign')
   }
 
