ba0c629ba8e78c10b631ea5ea62446f6fbbe8324
Ruan Silva <progmruansilva@gmail.com>
2023-09-13 14:43:32 -0300

    ✨ (development): Categories gets
    adicionado dois parametros ao categories, get e getById

--- api/services/categories.js
+++ api/services/categories.js
@@ -1,14 +1,18 @@
-module.exports = app => {
+module.exports = (app) => {
 
-const get = (req, res) => {
-  app.db('categories')
-    .select('name')
-    .then(author => res.json(author))
-  }
+  const get = (req, res) => {
+    app.db("categories")
+      .then((categories) => res.json(categories))
+      .catch((err) => res.status(500).send(err));
+  };
 
+  const getById = (req, res) => {
+    app.db("categories")
+      .where({ id: req.params.id })
+      .first()
+      .then((category) => res.json(category))
+      .catch((err) => res.status(500).send(err));
+  };
 
-
-
-
-  return { get }
-}
+  return { get, getById };
+};
