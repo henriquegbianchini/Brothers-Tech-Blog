cfb46b0937ab750a221065358e7263e7bd96c594
Ruan Silva <progmruansilva@gmail.com>
2023-09-13 16:16:47 -0300

    ✨ (development): Categories save method
    Criando metodo save em catigories.js, checagem de id e name, se passa adiciona ao banco  com put

--- api/services/categories.js
+++ api/services/categories.js
@@ -1,5 +1,4 @@
 module.exports = (app) => {
-
   const get = (req, res) => {
     app.db("categories")
       .then((categories) => res.json(categories))
@@ -14,5 +13,38 @@ module.exports = (app) => {
       .catch((err) => res.status(500).send(err));
   };
 
-  return { get, getById };
+  const save = (req, res) => {
+
+    function existsOrError(value, msg) {
+        if(!value) throw msg
+        if(Array.isArray(value) && value.length === 0) throw msg
+        if(typeof value === 'string' && !value.trim()) throw msg
+    }
+
+    const category = {
+      id: req.body.id,
+      name: req.body.name,
+    };
+
+    try {
+      existsOrError(category.name, "Nome não informado");
+    } catch (msg) {
+      return res.status(400).send(msg);
+    }
+
+    if (category.id) {
+      app.db("categories")
+        .update(category)
+        .where({ id: category.id })
+        .then((_) => res.status(204).send())
+        .catch((err) => res.status(500).send(err));
+    } else {
+      app.db("categories")
+        .insert(category)
+        .then((_) => res.status(204).send())
+        .catch((err) => res.status(500).send(err));
+    }
+  };
+
+  return { get, getById, save };
 };
